0137ce655661c5a7d4da68bae2cc9d16d2dc4211
Linh <linh@Felicias-iMac.Summit>
2020-05-01 18:07:27 -0700

    Activity Indicator

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Main from './components/MainComponent';
 import {YellowBox} from 'react-native';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
 YellowBox.ignoreWarnings(['Warming: ...'])
 console.disableYellowBox=true
 
+const store = ConfigureStore();
+
 export default function App() {
-	return <Main />;
+	return (<Provider store={store}>
+				<Main />
+			  </Provider>
+			);
 }
