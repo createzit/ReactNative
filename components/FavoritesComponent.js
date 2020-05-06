7edc072834a9280e91a63fbacd16ce2d70cc8ed6
Linh <linh@Felicias-iMac.Summit>
2020-05-06 15:15:57 -0700

    Swipeout and Delete Favorites

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,6 +4,8 @@ import {ListItem} from 'react-native-elements';
 import {connect} from 'react-redux';
 import {Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import {deleteFavorite} from '../redux/ActionCreators';
 
 const mapStateToProps = state =>{
     return{
@@ -12,7 +14,9 @@ const mapStateToProps = state =>{
     };
 };
 
-
+const mapDispatchToProps ={
+    deleteFavorite: campsiteId=>(deleteFavorite(campsiteId))
+}
 
 class Favorites extends Component {
 
@@ -24,13 +28,23 @@ class Favorites extends Component {
     render() {
         const {navigate}=this.props.navigation;
         const renderFavoriteItem = ({item}) =>{
+
+            const rightButton=[{
+                text: 'Delete',
+                type: 'delete',
+                onPress: () => this.props.deleteFavorite(item.id)
+
+            }];
             return(
-                <ListItem
-                title={item.name}
-                subtitle={item.description}
-                leftAvatar = {{source: {uri: baseUrl+item.image}}}
-                onPress={() =>  navigate('CamapsiteInfo', {campsiteId: item.id})}
-                />
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar = {{source: {uri: baseUrl+item.image}}}
+                    onPress={() =>  navigate('CamapsiteInfo', {campsiteId: item.id})}
+                    />
+                </Swipeout>
+                
             )
         }
         if (this.props.campsites.isloading){
@@ -54,4 +68,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
