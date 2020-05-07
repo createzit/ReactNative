dc719acfa1d2144284f9ff532fa3eb8e28a00cf1
Linh <linh@Felicias-iMac.Summit>
2020-05-07 10:48:06 -0700

    add animatable to all components

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,7 +3,8 @@ import {ScrollView, Text, FlatList} from 'react-native';
 import { Card ,ListItem} from 'react-native-elements';
 import {connect} from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-import Loading from './LoadingComponent'
+import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -62,11 +63,13 @@ class About extends Component {
         if (this.props.partners.errMess) {
             return (
                 <ScrollView>
-                    <Mission />
-                    <Card
-                        title='Community Partners'>
-                        <Text>{this.props.partners.errMess}</Text>
-                    </Card>
+                    <Animatable.view animation = 'fadeInDown' duration={2000} delay={1000}>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                    </Animatable.view>
                 </ScrollView>
             );
         }
@@ -74,10 +77,12 @@ class About extends Component {
         return (
             
             <ScrollView>
-              <Mission />
-              <Card title='Community Partners'>
-              <FlatList data={this.props.partners.partners} renderItem={renderPartner} keyExtractor={(item) => item.id.toString()} />
-              </Card>
+                <Animatable.View animation='fadeInDown' durartion={2000} delay={1000}>
+                    <Mission />
+                    <Card title='Community Partners'>
+                    <FlatList data={this.props.partners.partners} renderItem={renderPartner} keyExtractor={(item) => item.id.toString()} />
+                    </Card>
+              </Animatable.View>
             </ScrollView>
             
         )
